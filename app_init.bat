8b01869d30e28d20fd53b3e4d908ccfdcc1a30b8
Tomasz Sapletta <tom-sapletta-com@users.noreply.github.com>
2018-12-08 14:26:44 +0100

    Create app_init.bat

---
+++ app_init.bat
@@ -0,0 +1,8 @@
+## Install dependencies 
+npm i
+## Start the MongoDB service
+docker/docker-compose up
+## Start server 
+gulp develop
+## Api documentation can be found on: 
+curl http://localhost:3000/api-docs
